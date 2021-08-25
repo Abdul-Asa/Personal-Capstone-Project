64f66e2367886daea0eee5e20f0d0165570c4615
Abdullah Shehu <bigtulahi1@gmail.com>
2021-08-25 01:50:38 +0100

    kinda responsive navbar

--- client/src/components/dashboard/Home.js
+++ client/src/components/dashboard/Home.js
@@ -1,15 +1,33 @@
 import React from 'react';
+// import axios from 'axios';
 import Dashboard from './Dashboard';
+import Footer from '../landing page/Footer';
 import { getUser } from '../../utils/Common';
+// import { config } from '../../config';
 
 const Home = () => {
   const user = getUser();
+  // const { BASEURL } = config;
+  //   let header = {
+  //     headers: {
+  //       'auth-token': user.token,
+  //     },
+  //   };
+  // const testAuth = () => {
+  //   axios.get(`${BASEURL}/private`, header).then((response) => {
+  //     console.log(response);
+  //   });
+  // };
   return (
     <div>
       <Dashboard />
       <div className="container">
         <h1>Welcome {user.name}</h1>
       </div>
+      {/* <button className="btn-primary" onClick={testAuth}>
+        Test
+      </button> */}
+      <Footer />
     </div>
   );
 };
--- client/src/components/landing page/Navbar.js
+++ client/src/components/landing page/Navbar.js
@@ -4,9 +4,18 @@ const Navigation = () => {
   return (
     <nav className="navbar">
       <div className="container">
-        <a className="logo" href="/">
-          <i className="fas fa-parking"></i> <h1>Padrone</h1>
-        </a>
+        <div className="logo-nav-wrap">
+          <a className="logo" href="/">
+            <i className="fas fa-parking"></i> <h1>Padrone</h1>
+          </a>
+          {/* <div className="links-container">
+            <ul className="nav-links">
+              <li className="nav-link">About</li>
+              <li className="nav-link">Sponsor</li>
+            </ul>
+          </div> */}
+        </div>
+
         <div className="btn-container">
           <a href="/signup">
             <button className="btn">Sign up</button>
